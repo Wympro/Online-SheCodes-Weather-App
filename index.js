88cb093fd22956ab14d62be81cfbb427dcb93a49
Wympro <94883656+Wympro@users.noreply.github.com>
2022-01-01 16:12:49 -0700

    Increasing icon size

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function showTemperature(response) {
   let iconShow = document.querySelector("#Currenticon");
   iconShow.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
   );
 }
 
